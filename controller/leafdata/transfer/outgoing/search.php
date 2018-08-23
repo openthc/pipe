b93285a4be47b8870c352b701a9d7a1883137980
OpenTHC <code@openthc.org>
2018-08-23 17:43:23 +0000

    LeafData QA and Transfer

--- controller/leafdata/transfer/outgoing/search.php
+++ controller/leafdata/transfer/outgoing/search.php
@@ -3,35 +3,98 @@
 	Return a List of Outgoing Transfer Objects
 */
 
+use Edoceo\Radix\DB\SQL;
 
-$rbe = \RCE::factory($_SESSION['rbe']);
+$ret_code = 304;
 
-$t = $rbe->transfer();
-//var_dump($t);
+$obj_name = 'transfer';
 
-$out_list = array();
-$res = $rbe->transfer()->all();
-$src_list = $res['result']['data'];
-//var_dump($src_list);
-//exit(0);
+$sql_file = sprintf('%s/var/%s.sqlite', APP_ROOT, $_SESSION['sql-hash']);
+SQL::init('sqlite:' . $sql_file);
 
-foreach ($src_list as $src) {
+$dt0 = $_SERVER['REQUEST_TIME'];
+$sql = sprintf("SELECT val FROM _config WHERE key = 'sync-{$obj_name}-time'");
+$dt1 = intval(SQL::fetch_one($sql));
+$age = $dt0 - $dt1;
 
-	$out = array();
-	$out['guid'] = $src['global_id'];
-	$out['source_license_guid'] = $src['global_from_mme_id'];
-	$out['target_license_guid'] = $src['global_to_mme_id'];
-	$out['type'] = $src['manifest_type'];
-	$out['status'] = $src['status'];
-	$out['status_void'] = $src['void'];
-	//$out['_meta'] = $src;
-	//$out['carrier_license_guid'] = $src['global_transporting_mme_id'];
+if ($age >= 240) {
 
-	$out_list[] = $out;
+	$sql = "SELECT guid, hash FROM {$obj_name}";
+	$res_cached = SQL::fetch_mix($sql);
+
+	$rbe = \RCE::factory($_SESSION['rbe']);
+
+	$res_source = $rbe->transfer()->all();
+	if ('success' != $res_source['status']) {
+		return $RES->withJSON(array(
+			'status' => 'failure',
+			'detail' => $rbe->formatError($res_source),
+		), 500);
+	}
+
+	$res_source = $res_source['result']['data'];
+
+} else {
+
+	// From Cache
+	$res_cached = array();
+	$res_source = array();
+
+	$sql = "SELECT hash, meta FROM {$obj_name}";
+	$res = SQL::fetch_all($sql);
+
+	foreach ($res as $rec) {
+
+		$x = json_decode($rec['meta'], true);
+		$x['hash'] = $rec['hash'];
+
+		$res_cached[ $x['global_id'] ] = $x['hash'];
+		$res_source[] = $x;
+	}
+
 }
 
 
+foreach ($res_source as $src) {
+
+	if (empty($src['hash'])) {
+		$src['hash'] = _hash_obj($src);
+	}
+
+	$obj = array();
+	$obj['guid'] = $src['global_id'];
+	$obj['hash'] = $src['hash'];
+	$obj['source_license_guid'] = $src['global_from_mme_id'];
+	$obj['target_license_guid'] = $src['global_to_mme_id'];
+	$obj['type'] = $src['manifest_type'];
+	$obj['status'] = $src['status'];
+	$obj['status_void'] = $src['void'];
+	//$obj['carrier_license_guid'] = $src['global_transporting_mme_id'];
+
+	if ($obj['hash'] != $res_cached[ $obj['guid'] ]) {
+
+		$ret_code = 200;
+
+		$obj['_source'] = $src;
+		$obj['_updated'] = 1;
+
+		unset($src['hash']);
+
+		$sql = "INSERT OR REPLACE INTO {$obj_name} (guid, hash, meta) VALUES (:guid, :hash, :meta)";
+		$arg = array(
+			':guid' => $src['global_id'],
+			':hash' => $obj['hash'],
+			':meta' => json_encode($src),
+		);
+
+		SQL::query($sql, $arg);
+
+	}
+
+	$obj_output[] = $obj;
+}
+
 return $RES->withJSON(array(
 	'status' => 'success',
-	'result' => $out_list,
-));
+	'result' => $obj_output,
+), $ret_code, JSON_PRETTY_PRINT);
