b805a98e49e23cf960fb774a8290845b13765bf0
OpenTHC <code@openthc.org>
2018-09-24 01:14:03 +0000

    update stem routes

--- lib/Module/Stem.php
+++ lib/Module/Stem.php
@@ -3,28 +3,42 @@
 	Slim Module for the Stem interface
 */
 
+namespace App\Module;
+
 class Stem extends \OpenTHC\Module\Base
 {
 
-function __invoke($a)
-{
-	$a->get('', function($REQ, $RES, $ARG) {
-		return $this->view->render($RES, 'page/stem.html', array());
-	});
+	function __invoke($a)
+	{
+		$a->get('', function($REQ, $RES, $ARG) {
+			return $this->view->render($RES, 'page/stem.html', array());
+		});
 
-	$a->post('/biotrack', function($REQ, $RES, $ARG) {
-		return require_once(APP_ROOT . '/controller/stem/biotrack.php');
-	});
+		$a->map([ 'GET', 'POST' ], '/biotrack/{system}', function($REQ, $RES, $ARG) {
+			return require_once(APP_ROOT . '/controller/stem/biotrack.php');
+		})->add(array($this, '_load_rce'))->add('App\Middleware\Session');
 
-	$a->map([ 'GET', 'POST' ], '/leafdata/{path:.*}', function($REQ, $RES, $ARG) {
-		return require_once(APP_ROOT . '/controller/stem/leafdata.php');
-	});
+		$a->map([ 'GET', 'POST' ], '/leafdata/{system}/{path:.*}', function($REQ, $RES, $ARG) {
+			return require_once(APP_ROOT . '/controller/stem/leafdata.php');
+		});
 
-	$a->map([ 'GET', 'POST' ], '/metrc/{path:.*}', function($REQ, $RES, $ARG) {
-		return require_once(APP_ROOT . '/controller/stem/metrc.php');
-	});
+		$a->map([ 'GET', 'POST' ], '/metrc/{system}/{path:.*}', function($REQ, $RES, $ARG) {
+			return require_once(APP_ROOT . '/controller/stem/metrc.php');
+		});
 
-	//->add('App\Middleware\Log\HTTP');
-}
+	}
+
+	function _load_rce($REQ, $RES, $NMW)
+	{
+		if (empty($_SESSION['rbe-auth'])) {
+			die("NO");
+		}
+
+		var_dump($_SESSION);
+		exit;
+
+		return $NMW($REQ, $RES);
+
+	}
 
 }
