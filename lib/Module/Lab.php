c4744defbc0430477e032dcf17b02fc365048065
djbusby <code@edoceo.com>
2019-06-24 22:44:44 +0000

    Improve Laboratory Result Handling

---
+++ lib/Module/Lab.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * A Module contains a Slim Group
+ */
+
+namespace App\Module;
+
+class Lot extends \OpenTHC\Module\Base
+{
+	/**
+		@param $a App
+	*/
+	function __invoke($a)
+	{
+		$a->get('', function($REQ, $RES, $ARG) {
+			return _from_cre_file('lab/search.php', $RES, $ARG);
+		});
+
+		$a->get('/{guid}', function($REQ, $RES, $ARG) {
+			return _from_cre_file('lab/single.php', $RES, $ARG);
+		});
+
+	}
+}
