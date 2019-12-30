0603031eaf4d9e8a7c88f01edb993836228045a7
djbusby <code@edoceo.com>
2019-12-30 22:10:06 +0000

    update for stem/METRC

--- controller/stem/metrc.php
+++ controller/stem/metrc.php
@@ -1,37 +1,23 @@
 <?php
 /**
-	Stem pass-thru handler for METRC systems
-	Accept the Request, Sanatize It, Process Response and Sanatize Objects
-
-	To use the Passthru Configure this as the URL Base
-
-	https://pipe.openthc.com/stem/metrc
-
-	Forward to:
-		https://sandbox-api-ca.metrc.com/api/v1
-
-	Return Sanatized Response
-*/
+ * Stem pass-thru handler for METRC systems
+ */
 
 use Edoceo\Radix\DB\SQL;
 
-$cre_base = 'https://sandbox-api-ca.metrc.com';
-//$cre_base = 'http://localhost:8080';
-//if (!empty($_SERVER['HTTP_OPENTHC_CRE_BASE'])) {
-//	$cre_base = $_SERVER['HTTP_OPENTHC_CRE_BASE'];
-//}
+$cre_base = null;
 $cre_host = null;
 
 // Requested System
 switch ($ARG['system']) {
+case 'api-ak.metrc.com':
 case 'api-ca.metrc.com':
-//case 'sandbox-api-ca.metrc.com':
 case 'api-co.metrc.com':
-case 'sandbox-api-co.metrc.com':
+case 'api-mt.metrc.com':
 case 'api-nv.metrc.com':
-//case 'sandbox-api-nv.metrc.com':
 case 'api-or.metrc.com':
 case 'sandbox-api-or.metrc.com':
+case 'sandbox-api-co.metrc.com':
 	$cre_base = sprintf('https://%s', $ARG['system']);
 	break;
 default:
@@ -46,17 +32,16 @@ if (empty($cre_host)) {
 	$cre_host = parse_url($cre_base, PHP_URL_HOST);
 }
 
-// Auth
-// $RES = _req_auth($RES);
-// if (200 != $RES->getStatusCode()) {
-// 	return $RES;
-// }
-
 
 // Database
 $sql_hash = crc32($_SERVER['HTTP_AUTHORIZATION']);
-$sql_file = sprintf('%s/var/stem-metric-%08x.sqlite', APP_ROOT, $sql_hash);
+$sql_file = sprintf('%s/var/stem%s/leafdata-%08x.sqlite', APP_ROOT, date('Ymd'), $sql_hash);
+$sql_path = dirname($sql_file);
+if (!is_dir($sql_path)) {
+	mkdir($sql_path, 0755, true);
+}
 $sql_good = is_file($sql_file);
+
 
 SQL::init('sqlite:' . $sql_file);
 if (!$sql_good) {
@@ -75,16 +60,21 @@ $req_path = $cre_base . '/' . $src_path;
 
 // A cheap-ass, incomplete filter
 switch ($req_path) {
-case '/harvests/v1/active':
-case '/harvests/v1/onhold':
-case '/harvests/v1/inactive':
-case '/packages/v1/active':
-case '/patients/v1/active':
-case '/plantbatches/v1/active':
-case '/plants/v1/vegetative':
-case '/rooms/v1/active':
-case '/sales/v1/receipts':
-
+case '/facilities/v1':
+case '/harvests/v1/waste/types':
+case '/items/v1/categories':
+case '/labtests/v1/states':
+case '/labtests/v1/types':
+case '/packages/v1/types':
+case '/plantbatches/v1/types':
+case '/plants/v1/additives/types':
+case '/plants/v1/waste/methods':
+case '/sales/v1/customertypes':
+case '/unitsofmeasure/v1/active':
+	// These don't require a licenseNumber
+	break;
+default:
+	// Everything else does
 	if (empty($_GET['licenseNumber'])) {
 		// Fatal?
 		return $RES->withJSON(array(
@@ -104,6 +94,8 @@ $cre_http = new CRE_HTTP(array(
 
 // Forward
 switch ($_SERVER['REQUEST_METHOD']) {
+case 'DELETE':
+	break;
 case 'GET':
 
 	$req = new GuzzleHttp\Psr7\Request('GET', $req_path);
@@ -125,6 +117,19 @@ case 'POST':
 	$res = $cre_http->send($req, array('json' => $src_json));
 
 	break;
+
+case 'PUT':
+
+	$req = new GuzzleHttp\Psr7\Request('PUT', $req_path);
+	$req = $req->withHeader('host', $cre_host);
+	$req = $req->withHeader('authorization', $_SERVER['HTTP_AUTHORIZATION']);
+
+	$src_json = file_get_contents('php://input');
+
+	$res = $cre_http->send($req, array('json' => $src_json));
+
+	break;
+
 }
 
 // var_dump($res);
@@ -136,28 +141,3 @@ $RES = $RES->withHeader('content-type', $res->getHeader('content-type'));
 $RES = $RES->write($body);
 
 return $RES;
-
-
-/**
-	Authentication Validator
-*/
-function _req_auth($RES)
-{
-	$auth = $_SERVER['HTTP_AUTHORIZATION'];
-
-	if (empty($auth)) {
-		return $RES->withJSON(array(
-			'status' => 'failure',
-			'detail' => 'Invalid Auth [CSM#030]',
-		), 400);
-	}
-
-	if (!preg_match('/^Basic\s+(.+)$/', $auth, $m)) {
-		return $RES->withJSON(array(
-			'status' => 'failure',
-			'detail' => 'Invalid Auth [CSM#037]',
-		), 400);
-	}
-
-	return $RES;
-}
