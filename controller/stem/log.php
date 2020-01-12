48920a5b4829062cf3e304cbac674708ff6e247b
djbusby <code@edoceo.com>
2020-01-12 07:03:16 +0000

    improve METRC interfaces

--- controller/stem/log.php
+++ controller/stem/log.php
@@ -11,7 +11,7 @@ $hash = $_GET['hash'];
 
 $sql_file = sprintf('%s/var/stem%s/%s.sqlite', APP_ROOT, $date, $hash);
 if (!is_file($sql_file)) {
-	_exit_text('No File', 400);
+	_exit_text("No File: $sql_file", 400);
 }
 
 $dbc = new \Edoceo\Radix\DB\SQL('sqlite:' . $sql_file);
@@ -45,7 +45,7 @@ td {
 	font-size: 8pt;
 	vertical-align: top;
 }
-tr:hover {
+tr.tr1:hover {
 	background: #ff0;
 }
 tr.tr2 {
@@ -56,13 +56,16 @@ tr.tr3 {
 }
 </style>
 <script src="https://cdn.openthc.com/zepto/1.2.0/zepto.js" integrity="sha256-vrn14y7WH7zgEElyQqm2uCGSQrX/xjYDjniRUQx3NyU=" crossorigin="anonymous"></script>
-<!-- <script src="https://cdn.openthc.com/jquery/3.4.1/jquery.js"></script> -->
 </head>
 <body>
 <table>
 <?php
 $idx = 0;
-$res = $dbc->fetchAll('SELECT * FROM log_audit ORDER BY cts DESC limit 20');
+$sql = <<<SQL
+SELECT * FROM log_audit
+ORDER BY cts DESC limit 50
+SQL;
+$res = $dbc->fetchAll($sql);
 foreach ($res as $rec) {
 
 	$idx++;
@@ -73,21 +76,52 @@ foreach ($res as $rec) {
 	$res = strtok($rec['res'], "\n");
 
 	echo sprintf('<tr class="tr1" id="row-%d-1">', $idx);
-	echo '<td>' . _date('H:i:s', $rec['cts']) . '</td>';
+	echo '<td>' . _date('H:i:s.v', $rec['cts']) . '</td>';
 	echo '<td>' . h($req) . '</td>';
 	echo '<td>' . h($res) . '</td>';
 	echo '<td>' . $rec['code'] . '</td>';
+	// echo '<td>' . $rec['err'] . '</td>';
 	echo '</tr>';
+
+	$req = explode("\r\n\r\n", $rec['req']);
+	$req[0] = preg_replace('/^x\-mjf\-key:.+$/im', 'x-mjf-key: ********', $req[0]);
+	$req[0] = preg_replace('/^authorization:.+$/im', 'authorization: ********', $req[0]);
+	// if ('pretty' == $_GET['f']) {
+	// 	if (!empty($req[1])) {
+	// 		$req[1] = json_decode($req[1], true);
+	// 		$req[1] = json_encode($req[1], JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+	// 	}
+	// }
 
 	echo sprintf('<tr class="tr2" id="row-%d-2">', $idx);
 	echo '<td colspan="4">';
-	echo '<pre>' . h($rec['req']) . '</pre>';
+	echo '<pre>';
+	echo h($req[0]);
+	echo "\n";
+	// echo '\r\n';
+	echo "\n";
+	echo h($req[1]);
+	echo '</pre>';
 	echo '</td>';
 	echo '</tr>';
 
+	$res = explode("\r\n\r\n", $rec['res']);
+	if ('pretty' == $_GET['f']) {
+		if (!empty($res[1])) {
+			$res[1] = json_decode($res[1], true);
+			$res[1] = json_encode($res[1], JSON_NUMERIC_CHECK | JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
+		}
+	}
+
 	echo sprintf('<tr class="tr3" id="row-%d-3">', $idx);
 	echo '<td colspan="4">';
-	echo '<pre>' . h($rec['res']) . '</pre>';
+	echo '<pre>';
+	echo h($res[0]);
+	echo "\n";
+	// echo '\r\n';
+	echo "\n";
+	echo h($res[1]);
+	echo '</pre>';
 	echo '</td>';
 	echo '</tr>';
 
@@ -103,10 +137,15 @@ foreach ($res as $rec) {
 ?>
 </table>
 <script>
+var tr1 = null;
 var tr2 = null;
 var tr3 = null;
 $(function() {
 	$('.tr1').on('click', function() {
+
+		var id1 = this.id;
+		var id2 = id1.replace(/-1$/, '-2');
+		var id3 = id1.replace(/-1$/, '-3');
 
 		if (tr2) {
 			tr2.hide();
@@ -115,9 +154,12 @@ $(function() {
 			tr3.hide();
 		}
 
-		var id1 = this.id;
-		var id2 = id1.replace(/-1$/, '-2');
-		var id3 = id1.replace(/-1$/, '-3');
+		if (tr1 === this) {
+			tr1 = null;
+			return;
+		}
+
+		tr1 = this;
 
 		tr2 = $('#' + id2);
 		tr2.show();
--- controller/stem/metrc.php
+++ controller/stem/metrc.php
@@ -13,18 +13,24 @@ switch ($ARG['system']) {
 case 'api-ak.metrc.com':
 case 'api-ca.metrc.com':
 case 'api-co.metrc.com':
+case 'api-la.metrc.com':
+case 'api-ma.metrc.com':
+case 'api-md.metrc.com':
+case 'api-mi.metrc.com':
+case 'api-mo.metrc.com':
 case 'api-mt.metrc.com':
 case 'api-nv.metrc.com':
+case 'api-oh.metrc.com':
 case 'api-or.metrc.com':
 case 'sandbox-api-or.metrc.com':
 case 'sandbox-api-co.metrc.com':
 	$cre_base = sprintf('https://%s', $ARG['system']);
 	break;
 default:
-	_exit_json(array(
-		'status' => 'failure',
-		'detail' => 'CRE Not Found',
-	), 404);
+	_exit_json([
+		'data' => null,
+		'meta' => [ 'detail' => 'CRE Not Found' ]
+	], 404);
 }
 
 // From URL if not already set
@@ -35,7 +41,7 @@ if (empty($cre_host)) {
 
 // Database
 $sql_hash = crc32($_SERVER['HTTP_AUTHORIZATION']);
-$sql_file = sprintf('%s/var/stem%s/leafdata-%08x.sqlite', APP_ROOT, date('Ymd'), $sql_hash);
+$sql_file = sprintf('%s/var/stem%s/metrc-%08x.sqlite', APP_ROOT, date('Ymd'), $sql_hash);
 $sql_path = dirname($sql_file);
 if (!is_dir($sql_path)) {
 	mkdir($sql_path, 0755, true);
@@ -45,9 +51,8 @@ $sql_good = is_file($sql_file);
 
 SQL::init('sqlite:' . $sql_file);
 if (!$sql_good) {
-	SQL::query("CREATE TABLE log_audit (cts not null default (strftime('%s','now')), code, path, req, res, err)");
+	SQL::query("CREATE TABLE log_audit (cts not null default (strftime('%Y-%m-%d %H:%M:%f')), code, path, req, res, err)");
 }
-
 
 // Resolve Path
 $src_trim = sprintf('/stem/metrc/%s', $ARG['system']); // Stuff to remove
@@ -59,18 +64,18 @@ $src_path = ltrim($src_path, '/');
 $req_path = $cre_base . '/' . $src_path;
 
 // A cheap-ass, incomplete filter
-switch ($req_path) {
-case '/facilities/v1':
-case '/harvests/v1/waste/types':
-case '/items/v1/categories':
-case '/labtests/v1/states':
-case '/labtests/v1/types':
-case '/packages/v1/types':
-case '/plantbatches/v1/types':
-case '/plants/v1/additives/types':
-case '/plants/v1/waste/methods':
-case '/sales/v1/customertypes':
-case '/unitsofmeasure/v1/active':
+switch ($src_path) {
+case 'facilities/v1':
+case 'harvests/v1/waste/types':
+case 'items/v1/categories':
+case 'labtests/v1/states':
+case 'labtests/v1/types':
+case 'packages/v1/types':
+case 'plantbatches/v1/types':
+case 'plants/v1/additives/types':
+case 'plants/v1/waste/methods':
+case 'sales/v1/customertypes':
+case 'unitsofmeasure/v1/active':
 	// These don't require a licenseNumber
 	break;
 default:
@@ -95,38 +100,29 @@ $cre_http = new CRE_HTTP(array(
 // Forward
 switch ($_SERVER['REQUEST_METHOD']) {
 case 'DELETE':
+	_exit_text('Not Implemented', 501);
 	break;
 case 'GET':
 
 	$req = new GuzzleHttp\Psr7\Request('GET', $req_path);
-	$req = $req->withHeader('host', $cre_host);
 	$req = $req->withHeader('authorization', $_SERVER['HTTP_AUTHORIZATION']);
+	$req = $req->withHeader('host', $cre_host);
 
 	$res = $cre_http->send($req);
 
 	break;
 
 case 'POST':
-
-	$req = new GuzzleHttp\Psr7\Request('POST', $req_path);
-	$req = $req->withHeader('host', $cre_host);
-	$req = $req->withHeader('authorization', $_SERVER['HTTP_AUTHORIZATION']);
-
-	$src_json = file_get_contents('php://input');
-
-	$res = $cre_http->send($req, array('json' => $src_json));
-
-	break;
-
 case 'PUT':
 
-	$req = new GuzzleHttp\Psr7\Request('PUT', $req_path);
-	$req = $req->withHeader('host', $cre_host);
-	$req = $req->withHeader('authorization', $_SERVER['HTTP_AUTHORIZATION']);
-
 	$src_json = file_get_contents('php://input');
 
-	$res = $cre_http->send($req, array('json' => $src_json));
+	$req = new GuzzleHttp\Psr7\Request($_SERVER['REQUEST_METHOD'], $req_path);
+	$req = $req->withHeader('authorization', $_SERVER['HTTP_AUTHORIZATION']);
+	$req = $req->withHeader('content-type', 'application/json');
+	$req = $req->withHeader('host', $cre_host);
+
+	$res = $cre_http->send($req, array('body' => $src_json));
 
 	break;
 
